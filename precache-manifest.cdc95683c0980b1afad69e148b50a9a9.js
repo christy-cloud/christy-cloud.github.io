21718cbe915d75d6ce0719b857c4915c47a5a4c3
Christy Remijio <71412272+christy-cloud@users.noreply.github.com>
2020-12-14 15:05:54 -0500

    Updates

---
+++ precache-manifest.cdc95683c0980b1afad69e148b50a9a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91639017d7a4db2e0a2f5d8ef449f19a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed8774bcc506f11b17b7",
+    "url": "/static/css/main.eaa55f21.chunk.css"
+  },
+  {
+    "revision": "7dd5bd1d62977c921fef",
+    "url": "/static/js/2.4364ce75.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.4364ce75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed8774bcc506f11b17b7",
+    "url": "/static/js/main.366c8de7.chunk.js"
+  },
+  {
+    "revision": "ec6dab3b272866863c45",
+    "url": "/static/js/runtime-main.77e2c90b.js"
+  }
+]);
